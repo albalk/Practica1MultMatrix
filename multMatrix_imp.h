1e20543cf3fe600a2f0f53f25ca7d33ba8ece7df
albalk <allopcarru@gmail.com>
2023-11-09 17:30:21 +0100

    TERMINADO

--- multMatrix_imp.h
+++ multMatrix_imp.h
@@ -33,38 +33,28 @@ class multMatrix_imp{
         //recibe operaciones
         recvMSG(clientId, rpcIn);
         multMatrixOp operacion = unpack<multMatrixOp>(rpcIn);
-        //std::cout<<"Después de recibir mensaje\n";
 
         //ejecuta
         switch(operacion){
             case constructorOp:{
-                std::cout<<"\tConstructor paso3\n";
                 m=new multMatrix();
-                std::cout<<"\tConstructor paso4\n";
                 pack(rpcOut, (unsigned char)MSG_OK);
-                std::cout<<"\tConstructor paso5\n";
             }break;
             case destructorOp:{
                 delete m;
                 m=nullptr;
                 pack(rpcOut, (unsigned char)MSG_OK);
             }break;
-            case readMatrixOp:{ //Cambiar para recibir un string
+            case readMatrixOp:{
                 matrix_t* mres = new matrix_t();
-                std::cout<<"\tRead paso5\n";
                 int tam = unpack<int>(rpcIn);
                 char fileName[tam];
-                std::cout<<"\tRead paso6\n";
                 unpackv(rpcIn, (char*)fileName, tam);
-                std::cout<<"\tRead paso7\n";
                 mres = m->readMatrix(fileName);
-                std::cout<<"\tRead paso8\n";
 
                 pack(rpcOut, mres->rows);
                 pack(rpcOut, mres->cols);
-                std::cout<<"\tRead paso9\n";
                 packv(rpcOut, mres->data, mres->rows * mres->cols);
-                std::cout<<"\tRead paso10\n";
                 pack(rpcOut, (unsigned char)MSG_OK);
 
                 //liberar memoria
@@ -72,30 +62,28 @@ class multMatrix_imp{
             }break;
             case multMatricesOp:{
                 //crear matrices resultado, 1 y 2
-                std::cout<<"\tMult paso5\n";
                 matrix_t* mres = new matrix_t();
                 matrix_t* m1 = new matrix_t();
                 matrix_t* m2 = new matrix_t();
-                std::cout<<"\tMult paso6\n";
-                //desempaquetar el num rows y cols de m1
-                std::cout<<"\tMult paso7\n";
-                int numFilas1 = unpack<int>(rpcIn);
-                int numColumnas1 =  unpack<int>(rpcIn);
-                std::cout<<"\tMult paso8\n";
-                //desempaquetar el num rows y cols de m2
-                std::cout<<"\tMult paso9\n";
-                int numFilas2 = unpack<int>(rpcIn);
-                int numColumnas2 =  unpack<int>(rpcIn);
-                std::cout<<"\tMult paso10\n";
+
+                //desempaquetar el num rows, cols y data de m1
+                m1->rows = unpack<int>(rpcIn);
+                m1->cols =  unpack<int>(rpcIn);
+                m1->data=new int[m1->rows*m1->cols]; //resize
+                unpackv(rpcIn, m1->data, m1->rows * m1->cols);
+
+                //desempaquetar el num rows, cols y data de m2
+                m2->rows = unpack<int>(rpcIn);
+                m2->cols = unpack<int>(rpcIn);
+                m2->data=new int[m2->rows*m2->cols]; //resize
+                unpackv(rpcIn, m2->data, m2->rows * m2->cols);
+
                 //matriz resultado
                 mres = m->multMatrices(m1, m2);
-                std::cout<<"\tMult paso11\n";
                 
                 pack(rpcOut, (unsigned char)MSG_OK);
-                std::cout<<"\tMult paso12\n";
                 //pack(rpcOut, m->rows); no es necesario porque ya las tenemos (se envian por parametros)
                 //pack(rpcOut, m->cols);
-                std::cout<<"\tMult paso16\n";
                 packv(rpcOut, mres->data, mres->rows * mres->cols);
                 
                 //liberar memoria
@@ -107,20 +95,13 @@ class multMatrix_imp{
                 matrix_t* mres = new matrix_t();
                 mres->rows=unpack<int>(rpcIn);
                 mres->cols=unpack<int>(rpcIn);
+                mres->data=new int[mres->rows * mres->cols]; //resize
                 int tam = unpack<int>(rpcIn);
-                std::cout<<"\tWrite paso6\n";
+                
                 char fileName[tam];
                 unpackv(rpcIn, (char*)fileName, tam);
-                std::cout<<"\tWrite paso7\n";
                 m->writeMatrix(mres, fileName);
-                std::cout<<"\tWrite paso8\n";
-
-                pack(rpcOut, mres->rows);
-                pack(rpcOut, mres->cols);
-                std::cout<<"\tWrite paso9\n";
                 
-                packv(rpcOut, mres->data, mres->rows * mres->cols);
-                std::cout<<"\tWrite paso10\n";
                 pack(rpcOut, (unsigned char)MSG_OK);
 
                 //liberar memoria
@@ -143,33 +124,31 @@ class multMatrix_imp{
             case createRandMatrixOp:{
                 
                 matrix_t* mres = new matrix_t();
-                std::cout<<"\tRand paso4\n";
+
                 int numFilas = unpack<int>(rpcIn);
                 int numColumnas =  unpack<int>(rpcIn);
-                std::cout<<"\tRand paso5\n";
                 mres = m->createRandMatrix(numFilas, numColumnas);
-                std::cout<<"\tRand paso6\n";
+                    
                 pack(rpcOut, (unsigned char)MSG_OK);
                 //pack(rpcOut, m->rows); no es necesario porque ya las tenemos (se envian por parametros)
                 //pack(rpcOut, m->cols);
                 packv(rpcOut, mres->data, mres->rows * mres->cols);
-                std::cout<<"\tRand paso9\n";
+                    
                 //liberar memoria
                 freeMatrix(mres);
-                std::cout<<"\tRand paso10\n";
+                    
             }break;
             default:{
-                std::cout<<"Error: funcion no definida\n";
+                    
                 pack(rpcOut, (unsigned char)MSG_NOK);
             }break;
 
         }
         //devuelve resultados
-        std::cout<<"\tantes de enviar resultados\n";
+            
         sendMSG(clientId, rpcOut);
-        std::cout<<"\tdespues de enviar resultados\n";
+            
     }
 
-    //añadir free de los files
 
 };
--- multMatrix_stub.h
+++ multMatrix_stub.h
@@ -26,19 +26,16 @@ class multMatrix {
             std::vector<unsigned char> rpcOut;
             std::vector<unsigned char> rpcIn;
 
-            std::cout<<"\tConstructor paso1\n";
-
             pack(rpcOut, op);
-            std::cout<<"\tConstructor paso2\n";
+
             sendMSG(serverConection.serverId, rpcOut);
             recvMSG(serverConection.serverId, rpcIn);
-            std::cout<<"\tConstructor paso6\n";
+
             int ok = unpack<unsigned char>(rpcIn);
             //si no es ok error
             if(ok!=MSG_OK){
                 std::cout<<"Error"<<__FILE__<<":"<<__LINE__<<"\n";
             }
-            std::cout<<"\tConstructor paso7\n";
 
         };
 
@@ -67,34 +64,29 @@ class multMatrix {
                 multMatrixOp op=readMatrixOp;
                 std::vector<unsigned char> rpcOut;
                 std::vector<unsigned char> rpcIn;
-                std::cout<<"\tRead paso1\n";
                 
                 //empaquetar nombre de funcion
                 pack(rpcOut, op);
-                std::cout<<"\tRead paso2\n";
+
                 //empaquetar el tamaño del fichero
                 int tam = strlen(fileName)+1;
                 pack(rpcOut, tam);
-                std::cout<<"\tRead paso3\n";
+
                 //empaquetar nombre del fichero
                 packv(rpcOut, fileName, tam);
-                std::cout<<"\tRead paso4\n";
                 //enviar paquete
                 sendMSG(serverConection.serverId, rpcOut);
             
             //recibir numero de filas y columnas en la estructura
                 //recibir paquete
                 recvMSG(serverConection.serverId,  rpcIn);
-                std::cout<<"\tRead paso11\n";
                 //crear la estructura resultado
                 matrix_t* m=new matrix_t[1];
                 
                 m->rows=unpack<int>(rpcIn);
                 m->cols=unpack<int>(rpcIn);
                 m->data=new int [m->rows * m->cols];
-                std::cout<<"\tRead paso12\n";
                 unpackv(rpcIn, m->data, m->rows * m->cols);
-                std::cout<<"\tRead paso13\n";
 
             //rellenarla desempaquetando datos
                 //desempaquetar ok
@@ -114,48 +106,41 @@ class multMatrix {
                 multMatrixOp op=multMatricesOp;
                 std::vector<unsigned char> rpcOut;
                 std::vector<unsigned char> rpcIn;
-                std::cout<<"\tMult paso1\n";
+
                 //empaquetar nombre de funcion
                 pack(rpcOut, op);
-                std::cout<<"\tMult paso2\n";
+
                 //empaquetar filas y columnas de m1
                 pack(rpcOut, m1->rows);
                 pack(rpcOut, m1->cols);
-                std::cout<<"\tMult paso3\n";
+                packv(rpcOut, m1->data, m1->rows * m1->cols);
                 //empaquetar filas y columnas de m2
                 pack(rpcOut, m2->rows);
                 pack(rpcOut, m2->cols);
-                std::cout<<"\tMult paso4\n";
+                packv(rpcOut, m2->data, m2->rows * m2->cols);
                 //enviar paquete
                 sendMSG(serverConection.serverId, rpcOut);
 
             //recibir numero de filas y columnas en la estructura
                 //recibir paquete
                 recvMSG(serverConection.serverId,  rpcIn);
-                std::cout<<"\tMult paso13\n";
                 //crear la estructura resultado
                 matrix_t* m=new matrix_t[1];
                 //al multiplicar las dimensiones de la matriz resultado son el numfilas de m1 y numColumnas de m2
                 m->rows=m1->rows;
                 m->cols=m2->cols;
                 m->data=nullptr;
-                std::cout<<"\tMult paso14\n";
                 
 
             //rellenarla desempaquetando datos
                 //desempaquetar ok
                 int ok = unpack<unsigned char>(rpcIn);
-                std::cout<<"\tMult paso15\n";
 
                 //coger el tamaño
                     //redimensionar la variable
-                    std::cout<<"\tMult paso17\n";
                     m->data=new int[m1->rows*m2->cols];
-                    std::cout<<"\tMult paso18\n";
                     //desempaquetar
-                    std::cout<<"\tMult paso19\n";
                     unpackv(rpcIn, m->data, m->rows * m->cols);
-                    std::cout<<"\tMult paso20\n";
                 //si no es ok error
                 if(ok!=MSG_OK){
                     std::cout<<"Error"<<__FILE__<<":"<<__LINE__<<"\n";
@@ -170,33 +155,25 @@ class multMatrix {
                 multMatrixOp op=writeMatrixOp;
                 std::vector<unsigned char> rpcOut;
                 std::vector<unsigned char> rpcIn;
-                std::cout<<"\tWrite paso1\n";
+
                 //empaquetar nombre de funcion
                 pack(rpcOut, op);
-                std::cout<<"\tWrite paso2\n";
                 //empaquetar filas y columnas
                 pack(rpcOut, m->rows);
                 pack(rpcOut, m->cols);
-                std::cout<<"\tWrite paso3\n";
+
                 //empaquetar el tamaño del fichero
                 int tam = strlen(fileName)+1;
                 pack(rpcOut, tam);
-                std::cout<<"\tWrite paso4\n";
                 //empaquetar nombre del fichero
                 packv(rpcOut, fileName, tam);
-                std::cout<<"\tWrite paso5\n";
                 //enviar paquete
                 sendMSG(serverConection.serverId, rpcOut);
             
             //recibir numero de filas y columnas en la estructura
                 //recibir paquete
                 recvMSG(serverConection.serverId,  rpcIn);
-                std::cout<<"\tWrite paso11\n";
-                m->rows=unpack<int>(rpcIn);
-                m->cols=unpack<int>(rpcIn);
-                std::cout<<"\tWrite paso12\n";
-                unpackv(rpcIn, m->data, m->rows * m->cols);
-                std::cout<<"\tWrite paso13\n";
+                
                 //desempaquetar ok
                 int ok = unpack<unsigned char>(rpcIn);
                 //si no es ok error
@@ -253,21 +230,18 @@ class multMatrix {
                 multMatrixOp op=createRandMatrixOp;
                 std::vector<unsigned char> rpcOut;
                 std::vector<unsigned char> rpcIn;
-                std::cout<<"\tRand paso1\n";
+                    
                 //empaquetar nombre de funcion
                 pack(rpcOut, op);
-                std::cout<<"\tRand paso2\n";
                 //empaquetar filas y columnas
                 pack(rpcOut, rows);
                 pack(rpcOut, cols);
-                std::cout<<"\tRand paso3\n";
                 //enviar paquete
                 sendMSG(serverConection.serverId, rpcOut);
 
             //recibir numero de filas y columnas en la estructura
                 //recibir paquete
                 recvMSG(serverConection.serverId,  rpcIn);
-                std::cout<<"\tRand paso7\n";
                 //crear la estructura resultado
                 matrix_t* m=new matrix_t[1];
                 m->rows=rows;
@@ -276,16 +250,13 @@ class multMatrix {
 
             //rellenarla desempaquetando datos
                 //desempaquetar ok
-                std::cout<<"\tRand paso8\n";
                 int ok = unpack<unsigned char>(rpcIn);
                 
                 //coger el tamaño
                     //redimensionar la variable
                     m->data=new int[rows*cols];
                     //desempaquetar
-                    std::cout<<"\tRand paso11\n";
                     unpackv(rpcIn, m->data, m->rows * m->cols);
-                    std::cout<<"\tRand paso12\n";
                 
                 //si no es ok error
                 if(ok!=MSG_OK){
@@ -297,5 +268,4 @@ class multMatrix {
 
         };
 
-    //añadir free de los files
 };
