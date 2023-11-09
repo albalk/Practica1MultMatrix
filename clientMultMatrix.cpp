defd1ae760b393c8be44883badc883f65de6acb3
albalk <allopcarru@gmail.com>
2023-11-09 13:33:15 +0100

    depurar

--- clientMultMatrix.cpp
+++ clientMultMatrix.cpp
@@ -11,25 +11,38 @@ void freeMatrix(matrix_t* m){
 
 }
 int main()
-{
+{ 
+    std::cout<<"\tllamada Constructor\n";
     multMatrix* mmatrix=new multMatrix();
 
+    //std::cout<<"\tllamada RandMatrix\n"; //FUNCIONA
+    //matrix_t* m1= mmatrix->createRandMatrix(5,5);
 
-    matrix_t* m1= mmatrix->createRandMatrix(5,5);
-    matrix_t* m2= mmatrix->createIdentity(5,5);
-    matrix_t* mres=mmatrix->multMatrices(m1,m2);
-    mmatrix->writeMatrix(mres,"resultado.txt");
+    //std::cout<<"\tllamada Identity\n"; //FUNCIONA
+    //matrix_t* m2= mmatrix->createIdentity(5,5);
+
+    //std::cout<<"\tllamada multMatrix\n";
+    //matrix_t* mres=mmatrix->multMatrices(m1,m2); //ERROR UNPACKV FINAL
+
+    //std::cout<<"\tllamada writeMatrix\n";
+    //mmatrix->writeMatrix(mres,"resultado.txt");
+
+    std::cout<<"\tllamada readMatrix\n";
     matrix_t* m3=mmatrix->readMatrix("resultado.txt");
-    matrix_t* mres2=mmatrix->multMatrices(m1,m3);
-    mmatrix->writeMatrix(mres2,"resultado2.txt");
+    
+    std::cout<<"\tllamada writeMatrix\n";
+    mmatrix->writeMatrix(m3,"resultado2.txt");
+    
+    //matrix_t* mres2=mmatrix->multMatrices(m1,m3);
+    //mmatrix->writeMatrix(mres2,"resultado2.txt");
 
 
 
-    freeMatrix(m1);
-    freeMatrix(m2);
-    freeMatrix(mres);
-    freeMatrix(m3);
-    freeMatrix(mres2);
+    //freeMatrix(m1);
+    //freeMatrix(m2);
+    //freeMatrix(mres);
+    //freeMatrix(m3);
+    //freeMatrix(mres2);
     delete mmatrix;
 
 }
--- multMatrix_imp.h
+++ multMatrix_imp.h
@@ -38,8 +38,11 @@ class multMatrix_imp{
         //ejecuta
         switch(operacion){
             case constructorOp:{
+                std::cout<<"\tConstructor paso3\n";
                 m=new multMatrix();
+                std::cout<<"\tConstructor paso4\n";
                 pack(rpcOut, (unsigned char)MSG_OK);
+                std::cout<<"\tConstructor paso5\n";
             }break;
             case destructorOp:{
                 delete m;
@@ -47,16 +50,21 @@ class multMatrix_imp{
                 pack(rpcOut, (unsigned char)MSG_OK);
             }break;
             case readMatrixOp:{ //Cambiar para recibir un string
-                matrix_t* mres;
+                matrix_t* mres = new matrix_t();
+                std::cout<<"\tRead paso5\n";
                 int tam = unpack<int>(rpcIn);
                 char fileName[tam];
+                std::cout<<"\tRead paso6\n";
                 unpackv(rpcIn, (char*)fileName, tam);
+                std::cout<<"\tRead paso7\n";
                 mres = m->readMatrix(fileName);
+                std::cout<<"\tRead paso8\n";
 
                 pack(rpcOut, mres->rows);
                 pack(rpcOut, mres->cols);
-                
+                std::cout<<"\tRead paso9\n";
                 packv(rpcOut, mres->data, mres->rows * mres->cols);
+                std::cout<<"\tRead paso10\n";
                 pack(rpcOut, (unsigned char)MSG_OK);
 
                 //liberar memoria
@@ -64,21 +72,30 @@ class multMatrix_imp{
             }break;
             case multMatricesOp:{
                 //crear matrices resultado, 1 y 2
-                matrix_t* mres;
-                matrix_t* m1;
-                matrix_t* m2;
+                std::cout<<"\tMult paso5\n";
+                matrix_t* mres = new matrix_t();
+                matrix_t* m1 = new matrix_t();
+                matrix_t* m2 = new matrix_t();
+                std::cout<<"\tMult paso6\n";
                 //desempaquetar el num rows y cols de m1
+                std::cout<<"\tMult paso7\n";
                 int numFilas1 = unpack<int>(rpcIn);
                 int numColumnas1 =  unpack<int>(rpcIn);
+                std::cout<<"\tMult paso8\n";
                 //desempaquetar el num rows y cols de m2
+                std::cout<<"\tMult paso9\n";
                 int numFilas2 = unpack<int>(rpcIn);
                 int numColumnas2 =  unpack<int>(rpcIn);
+                std::cout<<"\tMult paso10\n";
                 //matriz resultado
                 mres = m->multMatrices(m1, m2);
+                std::cout<<"\tMult paso11\n";
                 
                 pack(rpcOut, (unsigned char)MSG_OK);
+                std::cout<<"\tMult paso12\n";
                 //pack(rpcOut, m->rows); no es necesario porque ya las tenemos (se envian por parametros)
                 //pack(rpcOut, m->cols);
+                std::cout<<"\tMult paso16\n";
                 packv(rpcOut, mres->data, mres->rows * mres->cols);
                 
                 //liberar memoria
@@ -87,23 +104,30 @@ class multMatrix_imp{
                 freeMatrix(m2);
             }break;
             case writeMatrixOp:{
-                matrix_t* mres;
+                matrix_t* mres = new matrix_t();
+                mres->rows=unpack<int>(rpcIn);
+                mres->cols=unpack<int>(rpcIn);
                 int tam = unpack<int>(rpcIn);
+                std::cout<<"\tWrite paso6\n";
                 char fileName[tam];
                 unpackv(rpcIn, (char*)fileName, tam);
-                m->writeMatrix(mres, fileName); //ERROR AL ENVIAR LOS PARAMETROS
+                std::cout<<"\tWrite paso7\n";
+                m->writeMatrix(mres, fileName);
+                std::cout<<"\tWrite paso8\n";
 
                 pack(rpcOut, mres->rows);
                 pack(rpcOut, mres->cols);
+                std::cout<<"\tWrite paso9\n";
                 
                 packv(rpcOut, mres->data, mres->rows * mres->cols);
+                std::cout<<"\tWrite paso10\n";
                 pack(rpcOut, (unsigned char)MSG_OK);
 
                 //liberar memoria
                 freeMatrix(mres);
             }break;
             case createIdentityOp:{
-                matrix_t* mres;
+                matrix_t* mres = new matrix_t();;
                 int numFilas = unpack<int>(rpcIn);
                 int numColumnas =  unpack<int>(rpcIn);
                 mres = m->createIdentity(numFilas, numColumnas);
@@ -117,19 +141,22 @@ class multMatrix_imp{
                 freeMatrix(mres);
             }break;
             case createRandMatrixOp:{
-
-                matrix_t* mres;
+                
+                matrix_t* mres = new matrix_t();
+                std::cout<<"\tRand paso4\n";
                 int numFilas = unpack<int>(rpcIn);
                 int numColumnas =  unpack<int>(rpcIn);
+                std::cout<<"\tRand paso5\n";
                 mres = m->createRandMatrix(numFilas, numColumnas);
-                
+                std::cout<<"\tRand paso6\n";
                 pack(rpcOut, (unsigned char)MSG_OK);
                 //pack(rpcOut, m->rows); no es necesario porque ya las tenemos (se envian por parametros)
                 //pack(rpcOut, m->cols);
                 packv(rpcOut, mres->data, mres->rows * mres->cols);
-                
+                std::cout<<"\tRand paso9\n";
                 //liberar memoria
                 freeMatrix(mres);
+                std::cout<<"\tRand paso10\n";
             }break;
             default:{
                 std::cout<<"Error: funcion no definida\n";
@@ -138,7 +165,9 @@ class multMatrix_imp{
 
         }
         //devuelve resultados
+        std::cout<<"\tantes de enviar resultados\n";
         sendMSG(clientId, rpcOut);
+        std::cout<<"\tdespues de enviar resultados\n";
     }
 
     //añadir free de los files
